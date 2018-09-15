5bb8f0e6a7859f010d4f31f496e18cab1badd8ab
Akhil Raja <p.akhilraja@gmail.com>
2018-09-15 18:34:23 +0530

    Added Java Library

---
+++ joketeller/src/main/java/com/example/joketeller/JokeProvider.java
@@ -0,0 +1,9 @@
+package com.example.joketeller;
+
+public class JokeProvider {
+
+    public static String tellJokeMethod () {
+        return "Joke  : Update this method to provide jokes";
+    }
+
+}
