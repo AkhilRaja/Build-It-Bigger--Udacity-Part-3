e76598f13ca34b51091fdbdd6788f864469fac0a
Akhil Raja <p.akhilraja@gmail.com>
2018-09-22 00:39:27 +0530

    Fixed the functional test

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/FunctionalTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/FunctionalTest.java
@@ -1,13 +1,8 @@
 package com.udacity.gradle.builditbigger;
 
-import android.app.Application;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.support.test.InstrumentationRegistry;
-import android.test.ApplicationTestCase;
-import android.test.suitebuilder.annotation.SmallTest;
-import android.util.Log;
-
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
@@ -35,31 +30,30 @@ public class FunctionalTest{
         final AsyncTask<Context, Void, String> asyncTask = new AsyncTask<Context, Void, String>() {
             @Override
             protected String doInBackground(Context... params) {
-                if (myApiService == null) {  // Only do this once
+                if (myApiService == null) {
+
                     MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                             new AndroidJsonFactory(), null)
                             // options for running against local devappserver
                             // - 10.0.2.2 is localhost's IP address in Android emulator
                             // - turn off compression when running against local devappserver
-                            //.setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                            .setRootUrl("http://192.168.1.103:8080/_ah/api/")
+                            .setRootUrl("http://10.0.2.2:8080/_ah/api/")
                             .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                                 @Override
-                                public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) {
                                     abstractGoogleClientRequest.setDisableGZipContent(true);
                                 }
                             });
+
+
                     myApiService = builder.build();
                 }
-
                 try {
                     return myApiService.sayHi().execute().getData();
-                } catch (Exception e) {
-                    Log.d("task...", e.getMessage(), e);
-                    return null;
+                } catch (IOException e) {
+                    return e.getMessage();
                 }
             }
-
             @Override
             protected void onPostExecute(String s) {
                 super.onPostExecute(s);
@@ -76,7 +70,7 @@ public class FunctionalTest{
         });
 
         signal.await(10,TimeUnit.SECONDS);
-        assertEquals("asdasda",joke);
+        assertEquals("Joke  : Update this method to provide jokes",joke);
 
     }
 
