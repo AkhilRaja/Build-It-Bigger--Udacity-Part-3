876e74c1d23f4069334416f94f0ae69d3f004bc9
Akhil Raja <p.akhilraja@gmail.com>
2018-09-22 19:03:09 +0530

    Changed the functional test again to reflect RetreiveAsyncTask

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/FunctionalTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/FunctionalTest.java
@@ -18,54 +18,23 @@ import java.util.concurrent.TimeUnit;
 import static junit.framework.Assert.assertEquals;
 
 
-public class FunctionalTest{
+public class FunctionalTest implements RetriveJokeAsyncTask.GotJokeCallback{
 
     private String joke ;
-    private static MyApi myApiService = null;
-
+    //private static MyApi myApiService = null;
+    private CountDownLatch signal;
 @Test
     public void RetrieveJoke() throws InterruptedException {
         //allow to wait one thread till other thread completes
-        final CountDownLatch signal = new CountDownLatch(1);
-        final AsyncTask<Context, Void, String> asyncTask = new AsyncTask<Context, Void, String>() {
-            @Override
-            protected String doInBackground(Context... params) {
-                if (myApiService == null) {
+        signal   = new CountDownLatch(1);
 
-                    MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                            new AndroidJsonFactory(), null)
-                            // options for running against local devappserver
-                            // - 10.0.2.2 is localhost's IP address in Android emulator
-                            // - turn off compression when running against local devappserver
-                            .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                            .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                                @Override
-                                public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) {
-                                    abstractGoogleClientRequest.setDisableGZipContent(true);
-                                }
-                            });
-
-
-                    myApiService = builder.build();
-                }
-                try {
-                    return myApiService.sayHi().execute().getData();
-                } catch (IOException e) {
-                    return e.getMessage();
-                }
-            }
-            @Override
-            protected void onPostExecute(String s) {
-                super.onPostExecute(s);
-                joke = s;
-                signal.countDown();
-            }
-        };
-
+        final FunctionalTest functionalTest = this;
         InstrumentationRegistry.getInstrumentation().runOnMainSync(new Runnable() {
             @Override
             public void run() {
-                asyncTask.execute();
+                RetriveJokeAsyncTask retriveJokeAsyncTask = new RetriveJokeAsyncTask();
+                retriveJokeAsyncTask.execute(functionalTest);
+
             }
         });
 
@@ -75,5 +44,10 @@ public class FunctionalTest{
     }
 
 
+    @Override
+    public void done(String result, boolean error) {
+            joke = result;
+            signal.countDown();
+    }
 }
 
